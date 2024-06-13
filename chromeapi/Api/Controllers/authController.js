feb24abb2097e9af84bb0577cb21fb1264f8add8
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-14 00:19:29 +0530

    added authcontroller

---
+++ chromeapi/Api/Controllers/authController.js
@@ -0,0 +1,119 @@
+const jwt = require("jsonwebtoken");
+const User = require("../Model/userModel");
+const Token = require("../Model/tokenModel");
+const Account = require("../Model/accountModel");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  })
+}
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+}
+
+exports.signUp = async (req, res, nex) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    address: req.body.address,
+    private_key: req.body.private_key,
+    mnemonic: req.body.mnemonic,
+  })
+  createSendToken(newUser, 201, req, res);
+}
+
+exports.login = async (req, res, next) => {
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    res.status(400).json({
+      status: "fail",
+      message: "Please provide email and password",
+    });
+  }
+
+  const user = await User.findOne({email}).select("+password");
+
+  if(!user || !(await user.correctPassword(password, user.password))){
+    res.status(401).json({
+      status: "fail",
+      message: "Incorrect email or password",
+    })
+  }
+
+  createSendToken(user, 200, req, res);
+
+}
+
+exports.allToken = async (req, res, next) => {
+  const tokens = await Token.find();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tokens,
+    }
+  })
+}
+
+exports.addToken = async (req, res, next) => {
+  const createToken = await Token.create({
+    name: req.body.name,
+    address: req.body.address,
+    symbol: req.body.symbol,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      createToken,
+    }
+  })
+}
+
+exports.addAccount = async (req, res, next) => {
+  const accounts = await Account.find();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      accounts,
+    }
+  })
+}
+
+exports.createAccount = async (req, res, next) => {
+  const account = await Account.create({
+    privateKey: req.body.privateKey,
+    address: req.body.address,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      account,
+    }
+  })
+}
--- chromeapi/Api/Model/userModel.js
+++ chromeapi/Api/Model/userModel.js
@@ -1,11 +1,71 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const tokenSchema = new mongoose.Schema({
-  name: String,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "Passwords are not same!"
+    },
+  },
   address: String,
-  Symbol: String,
+  private_key: String,
+  mnemonic: String,
 });
 
-const Token = mongoose.model("Token", tokenSchema);
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
 
-module.exports = Token
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre("save", function (next) {
+  if(!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+})
+
+userSchema.pre(/^find/, function (next) {
+  this.find({active: {$ne: false}});
+  next()
+})
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword,userPassword);
+}
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp){
+  if(this.passwordChangedAt){
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime()/1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false
+}
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
